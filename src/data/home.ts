8089b1414ae49389eea030a8ad78114821e987a9
melnicf <filipmelnic.dev@gmail.com>
2024-07-01 15:13:57 +0200

    refactor: standardize reusable styles and components
    -create data folder for website content per page

---
+++ src/data/home.ts
@@ -0,0 +1,17 @@
+export const services = [
+    {
+        title: "Car Repair",
+        description: "We offer professional car repair services to ensure your vehicle is in top condition.",
+        image:"/public/assets/circle1.png"
+    },
+    {
+        title: "Car Wash",
+        description: "We offer professional car wash services to ensure your vehicle is in top condition.",
+        image:"/public/assets/circle1.png"
+    },
+    {
+        title: "Car Cleaning",
+        description: "We offer professional car cleaning services to ensure your vehicle is in top condition.",
+        image:"/public/assets/circle1.png"
+    }
+]
