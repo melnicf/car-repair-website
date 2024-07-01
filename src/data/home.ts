eb81192365448c77544c58ecbff4cfed28a46e80
melnicf <filipmelnic.dev@gmail.com>
2024-07-01 15:27:17 +0200

    fix: change image url root to /assets/

--- src/data/home.ts
+++ src/data/home.ts
@@ -2,16 +2,16 @@ export const services = [
     {
         title: "Car Repair",
         description: "We offer professional car repair services to ensure your vehicle is in top condition.",
-        image:"/public/assets/circle1.png"
+        image:"/assets/circle1.png"
     },
     {
         title: "Car Wash",
         description: "We offer professional car wash services to ensure your vehicle is in top condition.",
-        image:"/public/assets/circle1.png"
+        image:"/assets/circle1.png"
     },
     {
         title: "Car Cleaning",
         description: "We offer professional car cleaning services to ensure your vehicle is in top condition.",
-        image:"/public/assets/circle1.png"
+        image:"/assets/circle1.png"
     }
 ]
