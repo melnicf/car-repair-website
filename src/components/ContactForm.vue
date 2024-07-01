46e6695d16e307be014e3a2cd64b60a06abc81af
melnicf <filipmelnic.dev@gmail.com>
2024-07-01 18:02:40 +0200

    feat: add ts and intellisense

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,100 +1,87 @@
 <template>
-  <div class="contact_form">
-    <div class="contact_form_info">
-      <h1>Contattaci!</h1>
-      <p>
-        Siamo a tua disposizione per qualsiasi informazione o richiesta di
-        assistenza. Il nostro team di esperti è pronto a rispondere a tutte le
-        tue domande riguardo ai nostri servizi e a fornirti supporto per
-        qualsiasi esigenza legata alla tua auto. Compila il modulo a destra
-        con i tuoi dettagli e la tua richiesta, e ti ricontatteremo al più
-        presto per offrirti il miglior servizio possibile. La tua
-        soddisfazione è la nostra priorità.
-      </p>
-    </div>
-    <form class="contact_form_fields" @submit.prevent="validateForm">
-      <div class="contact_form_top">
-        <InputField
-          v-model="form.name"
-          placeholder="Nome"
-          :error="errors.name"
-        />
-        <InputField
-          v-model="form.email"
-          placeholder="Email"
-          :error="errors.email"
-        />
-      </div>
-      <InputField
-        v-model="form.object"
-        placeholder="Oggetto"
-        :error="errors.object"
-      />
-      <TextAreaField
-        v-model="form.message"
-        placeholder="Messaggio"
-        :error="errors.message"
-      />
-      <div class="contact_form_button">
-        <PrimaryButton class="button_type_1" label="INVIA" type="submit"/>
-      </div>
-    </form>
-  </div>
+	<div class="contact_form">
+		<div class="contact_form_info">
+			<h1>Contattaci!</h1>
+			<p>
+				Siamo a tua disposizione per qualsiasi informazione o richiesta di assistenza. Il nostro
+				team di esperti è pronto a rispondere a tutte le tue domande riguardo ai nostri servizi e a
+				fornirti supporto per qualsiasi esigenza legata alla tua auto. Compila il modulo a destra
+				con i tuoi dettagli e la tua richiesta, e ti ricontatteremo al più presto per offrirti il
+				miglior servizio possibile. La tua soddisfazione è la nostra priorità.
+			</p>
+		</div>
+		<form class="contact_form_fields" @submit.prevent="validateForm">
+			<div class="contact_form_top">
+				<InputField v-model="form.name" placeholder="Nome" :error="errors.name" />
+				<InputField v-model="form.email" placeholder="Email" :error="errors.email" />
+			</div>
+			<InputField v-model="form.object" placeholder="Oggetto" :error="errors.object" />
+			<TextAreaField v-model="form.message" placeholder="Messaggio" :error="errors.message" />
+			<div class="contact_form_button">
+				<PrimaryButton class="button_type_1" label="INVIA" type="submit" />
+			</div>
+		</form>
+	</div>
 </template>
 
 <script lang="ts">
-import { reactive } from 'vue';
-import PrimaryButton from '../components/PrimaryButton.vue';
-import InputField from '../components/InputField.vue';
-import TextAreaField from '../components/TextAreaField.vue';
+	import { reactive } from 'vue'
+	import PrimaryButton from '../components/PrimaryButton.vue'
+	import InputField from '../components/InputField.vue'
+	import TextAreaField from '../components/TextAreaField.vue'
 
-export default {
-  components: {
-    PrimaryButton,
-    InputField,
-    TextAreaField
-  },
-  setup() {
-    const form = reactive({
-      name: '',
-      email: '',
-      object: '',
-      message: '',
-    });
+	export default {
+		components: {
+			PrimaryButton,
+			InputField,
+			TextAreaField
+		},
+		setup() {
+			const form = reactive({
+				name: '',
+				email: '',
+				object: '',
+				message: ''
+			})
 
-    const errors = reactive({
-      name: null,
-      email: null,
-      object: null,
-      message: null,
-    });
+			const errors = reactive<{
+				name: string | null
+				email: string | null
+				object: string | null
+				message: string | null
+			}>({
+				name: null,
+				email: null,
+				object: null,
+				message: null
+			})
 
-    const validateForm = () => {
-      errors.name = form.name ? null : 'Nome è richiesto';
-      errors.email = form.email ? null : 'Email è richiesta';
-      errors.object = form.object ? null : 'Oggetto è richiesto';
-      errors.message = form.message ? null : 'Messaggio è richiesto';
+			const validateForm = () => {
+				errors.name = form.name ? null : 'Nome è richiesto'
+				errors.email = form.email ? null : 'Email è richiesta'
+				errors.object = form.object ? null : 'Oggetto è richiesto'
+				errors.message = form.message ? null : 'Messaggio è richiesto'
 
-      if (!errors.name && !errors.email && !errors.object && !errors.message) {
-        // Form is valid, submit the form
-        console.log('Form submitted:', form);
-        // Add your form submission logic here
-      } else {
-        console.log('Form validation failed', errors);
-      }
-    };
+				if (!errors.name && !errors.email && !errors.object && !errors.message) {
+					// Form is valid, submit the form
+					console.log('Form submitted:', form)
+					// Add your form submission logic here
+				} else {
+					console.log('Form validation failed', errors)
+				}
+			}
 
-    return {
-      form,
-      errors,
-      validateForm,
-    };
-  },
-};
+			return {
+				form,
+				errors,
+				validateForm
+			}
+		}
+	}
 </script>
 
 <style scoped>
-.contact_form {
+	.contact_form {
   width: 70%;
   height: auto;
   padding: var(--padding-large);
