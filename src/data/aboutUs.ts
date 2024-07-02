6cd0a7e337d3f21d042eb5e14d71fd3c07a7322a
melnicf <filipmelnic.dev@gmail.com>
2024-07-02 15:14:10 +0200

    feat: add content to data folder
    -Added reusable component for the features

---
+++ src/data/aboutUs.ts
@@ -0,0 +1,25 @@
+export const aboutUs = {
+	title: 'Chi siamo',
+	description:
+		'Benvenuti nella nostra azienda, dove esperienza e passione si uniscono per offrire i migliori servizi. La nostra missione è garantire la soddisfazione del cliente attraverso soluzioni innovative e di alta qualità. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.',
+	features: [
+		{
+			title: 'La nostra storia',
+			description:
+				'Nella nostra storia, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.',
+			coverImage: '/assets/our-history.jpg',
+		},
+		{
+			title: 'La nostra squadra',
+			description:
+				'La nosce squadra, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.',
+			coverImage: '/assets/our-team.jpg',
+		},
+		{
+			title: 'I nostri valori',
+			description:
+				'I nosci valori, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.',
+			coverImage: '/assets/our-values.jpg',
+		},
+	],
+}
--- src/data/home.ts
+++ src/data/home.ts
@@ -1,17 +1,20 @@
 export const services = [
-    {
-        title: "Car Repair",
-        description: "We offer professional car repair services to ensure your vehicle is in top condition.",
-        image:"/assets/circle1.png"
-    },
-    {
-        title: "Car Wash",
-        description: "We offer professional car wash services to ensure your vehicle is in top condition.",
-        image:"/assets/circle1.png"
-    },
-    {
-        title: "Car Cleaning",
-        description: "We offer professional car cleaning services to ensure your vehicle is in top condition.",
-        image:"/assets/circle1.png"
-    }
+	{
+		title: 'Car Repair',
+		description:
+			'We offer professional car repair services to ensure your vehicle is in top condition.',
+		image: '/assets/circle1.png',
+	},
+	{
+		title: 'Car Wash',
+		description:
+			'We offer professional car wash services to ensure your vehicle is in top condition.',
+		image: '/assets/circle1.png',
+	},
+	{
+		title: 'Car Cleaning',
+		description:
+			'We offer professional car cleaning services to ensure your vehicle is in top condition.',
+		image: '/assets/circle1.png',
+	},
 ]
