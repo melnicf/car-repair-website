a2c3f72882eaeff590675d3fa28135151984e1da
melnicf <filipmelnic.dev@gmail.com>
2024-07-26 17:31:17 +0200

    feat: restyle about us page

--- src/data/aboutUs.ts
+++ src/data/aboutUs.ts
@@ -4,16 +4,18 @@ export const aboutUs = {
 		'Benvenuti nella nostra azienda, dove esperienza e passione si uniscono per offrire i migliori servizi. La nostra missione è garantire la soddisfazione del cliente attraverso soluzioni innovative e di alta qualità. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.',
 	features: [
 		{
-			title: 'La nostra storia',
+			title: 'La nostra ',
+			span: 'storia',
 			description:
 				'Nella nostra storia, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.Nella nostra storia, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.Nella nostra storia, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti. ',
-			coverImage: '/assets/our-history.jpg',
+			coverImage: '/assets/old_car.svg',
 		},
 		{
-			title: 'I nostri valori',
+			title: 'I nostri ',
+			span: 'valori',
 			description:
 				'I nosci valori, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.I nosci valori, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti. Ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.I nosci valori, ci impegniamo a migliorare continuamente e a superare le aspettative dei nostri clienti.',
-			coverImage: '/assets/our-values.jpg',
+			coverImage: '/assets/papyrus.svg',
 		},
 	],
 
