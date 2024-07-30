6e83fcf0a5b00e37760ddad4020f95a536dc1b1c
melnicf <filipmelnic.dev@gmail.com>
2024-07-31 01:59:35 +0200

    feat: refactor shadow mixin to use args

--- src/components/common/InputField.vue
+++ src/components/common/InputField.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="input_group">
-    <input
-      :name="name"
-      :type="type"
-      v-model="internalValue"
-      :placeholder="placeholder"
-    />
-    <span v-if="error" class="error">{{ error }}</span>
-  </div>
+    <div class="input_group">
+        <input
+            :name="name"
+            :type="type"
+            v-model="internalValue"
+            :placeholder="placeholder"
+        />
+        <span v-if="error" class="error">{{ error }}</span>
+    </div>
 </template>
 
 <script lang="ts">
@@ -15,40 +15,40 @@ import { defineComponent, computed } from "vue";
 import type { PropType } from "vue";
 
 export default defineComponent({
-  name: "InputField",
-  props: {
-    modelValue: {
-      type: String,
-      required: true,
+    name: "InputField",
+    props: {
+        modelValue: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String,
+            default: "text",
+        },
+        placeholder: {
+            type: String,
+            required: true,
+        },
+        error: {
+            type: String as PropType<string | null>,
+            default: null,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
     },
-    type: {
-      type: String,
-      default: "text",
-    },
-    placeholder: {
-      type: String,
-      required: true,
-    },
-    error: {
-      type: String as PropType<string | null>,
-      default: null,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-  },
-  emits: ["update:modelValue"],
-  setup(props, { emit }) {
-    const internalValue = computed({
-      get: () => props.modelValue,
-      set: (value: string) => emit("update:modelValue", value),
-    });
+    emits: ["update:modelValue"],
+    setup(props, { emit }) {
+        const internalValue = computed({
+            get: () => props.modelValue,
+            set: (value: string) => emit("update:modelValue", value),
+        });
 
-    return {
-      internalValue,
-    };
-  },
+        return {
+            internalValue,
+        };
+    },
 });
 </script>
 
@@ -56,27 +56,27 @@ export default defineComponent({
 @use "@styles/variables/variables.scss" as variables;
 
 .input_group {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-
-  input {
-    border-radius: variables.$border-radius;
-    color: variables.$quaternary;
-    height: 15px;
-    outline: none;
-    padding: variables.$padding-medium;
+    display: flex;
+    flex-direction: column;
     width: 100%;
-    border: none;
 
-    @include variables.shadow4;
-    @include variables.typo-fields1;
-  }
+    input {
+        border-radius: variables.$border-radius;
+        color: variables.$quaternary;
+        height: 15px;
+        outline: none;
+        padding: variables.$padding-medium;
+        width: 100%;
+        border: none;
 
-  .error {
-    color: red;
-    font-size: 14px;
-    margin-top: 5px;
-  }
+        @include variables.shadow(s, variables.$quaternary);
+        @include variables.typo-fields1;
+    }
+
+    .error {
+        color: red;
+        font-size: 14px;
+        margin-top: 5px;
+    }
 }
 </style>
--- src/components/common/TextAreaField.vue
+++ src/components/common/TextAreaField.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="input_group">
-    <textarea v-model="internalValue" :placeholder="placeholder"></textarea>
-    <span v-if="error" class="error">{{ error }}</span>
-  </div>
+    <div class="input_group">
+        <textarea v-model="internalValue" :placeholder="placeholder"></textarea>
+        <span v-if="error" class="error">{{ error }}</span>
+    </div>
 </template>
 
 <script lang="ts">
@@ -10,32 +10,32 @@ import { defineComponent, computed } from "vue";
 import type { PropType } from "vue";
 
 export default defineComponent({
-  name: "TextAreaField",
-  props: {
-    modelValue: {
-      type: String,
-      required: true,
+    name: "TextAreaField",
+    props: {
+        modelValue: {
+            type: String,
+            required: true,
+        },
+        placeholder: {
+            type: String,
+            required: true,
+        },
+        error: {
+            type: String as PropType<string | null>,
+            default: null,
+        },
     },
-    placeholder: {
-      type: String,
-      required: true,
-    },
-    error: {
-      type: String as PropType<string | null>,
-      default: null,
-    },
-  },
-  emits: ["update:modelValue"],
-  setup(props, { emit }) {
-    const internalValue = computed({
-      get: () => props.modelValue,
-      set: (value: string) => emit("update:modelValue", value),
-    });
+    emits: ["update:modelValue"],
+    setup(props, { emit }) {
+        const internalValue = computed({
+            get: () => props.modelValue,
+            set: (value: string) => emit("update:modelValue", value),
+        });
 
-    return {
-      internalValue,
-    };
-  },
+        return {
+            internalValue,
+        };
+    },
 });
 </script>
 
@@ -43,27 +43,27 @@ export default defineComponent({
 @use "@styles/variables/variables.scss" as variables;
 
 .input_group {
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-
-  textarea {
-    border-radius: variables.$border-radius;
-    color: variables.$quaternary;
-    height: 200px;
-    outline: none;
-    padding: variables.$padding-medium;
+    display: flex;
+    flex-direction: column;
     width: 100%;
-    border: none;
 
-    @include variables.shadow4;
-    @include variables.typo-fields1;
-  }
+    textarea {
+        border-radius: variables.$border-radius;
+        color: variables.$quaternary;
+        height: 200px;
+        outline: none;
+        padding: variables.$padding-medium;
+        width: 100%;
+        border: none;
 
-  .error {
-    color: red;
-    font-size: 14px;
-    margin-top: 5px;
-  }
+        @include variables.shadow(m, variables.$quaternary);
+        @include variables.typo-fields1;
+    }
+
+    .error {
+        color: red;
+        font-size: 14px;
+        margin-top: 5px;
+    }
 }
 </style>
