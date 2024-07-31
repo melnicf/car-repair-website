6a06417f21c89ed32b9ab9629d8a17e32eadeb19
melnicf <filipmelnic.dev@gmail.com>
2024-07-31 17:57:15 +0200

    feat: refactor contactUs to follow theme

---
+++ src/data/contactUs.ts
@@ -0,0 +1,30 @@
+export const contactUs = {
+    availability: [
+        { day: "Lunedì", open: "9:00 -", close: "17:00" },
+        { day: "Martedì", open: "9:00 -", close: "17:00" },
+        { day: "Mercoledì", open: "9:00 - ", close: "17:00" },
+        { day: "Giovedì", open: "9:00 - ", close: "17:00" },
+        { day: "Venerdì", open: "9:00 - ", close: "17:00" },
+        { day: "Sabato", open: "10:00 - ", close: "16:00" },
+        { day: "Domenica", open: "Chiuso", close: "" },
+    ],
+    contactNumbers: [
+        { label: "Primary", number: "340 123 4567" },
+        { label: "Secondary", number: "340 123 4567" },
+    ],
+    socialLinks: [
+        { platform: "Facebook", url: "https://facebook.com" },
+        { platform: "Twitter", url: "https://twitter.com" },
+        { platform: "Instagram", url: "https://instagram.com" },
+    ],
+    location: {
+        address: "20834 Nova Milanese MB",
+        mapEmbedUrl:
+            "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11168.284344614001!2d9.184663525549261!3d45.589115703337356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786beeae2418757%3A0x27e9f70a0c484a63!2s20834%20Nova%20Milanese%20MB!5e0!3m2!1sit!2sit!4v1719877020677!5m2!1sit!2sit",
+    },
+    heroTitle: {
+        paragraph: "Per informazioni o per un appuntamento",
+        icon: "fas fa-address-book",
+        title: "Modi per contattarci",
+    },
+};
