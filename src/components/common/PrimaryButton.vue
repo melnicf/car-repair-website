71ec1853c54748b0b77a185d934e769452f37458
melnicf <filipmelnic.dev@gmail.com>
2024-07-31 18:24:21 +0200

    feat: point buttons to relevant sections

--- src/components/common/PrimaryButton.vue
+++ src/components/common/PrimaryButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :class="class" :type="type">{{ label }}</button>
+    <button :class="class" :type="type" @click="navigate">{{ label }}</button>
 </template>
 
 <script lang="ts">
@@ -7,23 +7,48 @@ import { defineComponent } from "vue";
 import type { PropType } from "vue";
 
 export default defineComponent({
-  name: "PrimaryButton",
-  props: {
-    class: {
-      type: String as PropType<
-        "button_type_1" | "button_type_2" | "button_type_3"
-      >,
-      required: true,
+    name: "PrimaryButton",
+    props: {
+        class: {
+            type: String as PropType<
+                "button_type_1" | "button_type_2" | "button_type_3"
+            >,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+        type: {
+            type: String as PropType<"button" | "submit" | "reset">,
+            default: "button",
+        },
+        href: {
+            type: String,
+            default: "",
+        },
     },
-    label: {
-      type: String,
-      required: true,
+    setup(props) {
+        const navigate = () => {
+            if (props.href) {
+                if (props.href.startsWith("#")) {
+                    document
+                        .getElementById(props.href.substring(1))
+                        ?.scrollIntoView({
+                            behavior: "smooth",
+                        });
+                } else if (props.href.includes("#")) {
+                    window.location.href = props.href;
+                } else {
+                    window.location.href = props.href;
+                }
+            }
+        };
+
+        return {
+            navigate,
+        };
     },
-    type: {
-      type: String as PropType<"button" | "submit" | "reset">,
-      default: "button",
-    },
-  },
 });
 </script>
 
@@ -31,59 +56,59 @@ export default defineComponent({
 @use "@styles/variables/variables.scss" as variables;
 
 .button_type_1 {
-  padding: variables.$padding-small;
-  border-radius: variables.$border-radius;
-  background-color: variables.$primary;
-  transition: variables.$transition-ease;
-  outline: none;
-  border: none;
-  color: variables.$black;
-  width: 200px;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    padding: variables.$padding-small;
+    border-radius: variables.$border-radius;
+    background-color: variables.$primary;
+    transition: variables.$transition-ease;
+    outline: none;
+    border: none;
+    color: variables.$black;
+    width: 200px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  &:hover {
-    background-color: lighten(variables.$primary, 10%);
-  }
+    &:hover {
+        background-color: lighten(variables.$primary, 10%);
+    }
 }
 
 .button_type_2 {
-  padding: variables.$padding-small;
-  border-radius: variables.$border-radius;
-  background-color: variables.$tertiary;
-  transition: variables.$transition-ease;
-  outline: none;
-  border: 1px variables.$white solid;
-  color: variables.$black;
-  width: 200px;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    padding: variables.$padding-small;
+    border-radius: variables.$border-radius;
+    background-color: variables.$tertiary;
+    transition: variables.$transition-ease;
+    outline: none;
+    border: 1px variables.$white solid;
+    color: variables.$black;
+    width: 200px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  &:hover {
-    background-color: lighten(variables.$tertiary, 10%);
-  }
+    &:hover {
+        background-color: lighten(variables.$tertiary, 10%);
+    }
 }
 
 .button_type_3 {
-  padding: variables.$padding-small;
-  border-radius: variables.$border-radius;
-  background-color: variables.$secondary;
-  transition: variables.$transition-ease;
-  outline: none;
-  border: 1px variables.$white solid;
-  color: variables.$primary;
-  width: 200px;
-  cursor: pointer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    padding: variables.$padding-small;
+    border-radius: variables.$border-radius;
+    background-color: variables.$secondary;
+    transition: variables.$transition-ease;
+    outline: none;
+    border: 1px variables.$white solid;
+    color: variables.$primary;
+    width: 200px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  &:hover {
-    background-color: lighten(variables.$quaternary, 10%);
-  }
+    &:hover {
+        background-color: lighten(variables.$quaternary, 10%);
+    }
 }
 </style>
