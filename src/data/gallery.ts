c54d8e3a9ef7e00acc095540e017e7fa4a58e367
melnicf <filipmelnic.dev@gmail.com>
2024-07-31 16:47:00 +0200

    refactor: extract hero title logic to parent component

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,5 +1,5 @@
 export const gallery = {
-	title: 'Galleria',
-	paragraph:
-		'Benvenuti nella nostra galleria dedicata al servizio di riparazione auto! Qui potrete esplorare una raccolta di immagini che mostrano il nostro lavoro quotidiano, dalle riparazioni di precisione ai restauri completi. Ogni foto racconta una storia di dedizione e professionalità, evidenziando la qualità del nostro servizio e la soddisfazione dei nostri clienti. Siamo fieri di condividere con voi il nostro impegno per mantenere le vostre auto in perfette condizioni. Buona visione!',
-}
+    title: "Le nostre Immagini",
+    paragraph:
+        "Benvenuti nella nostra galleria dedicata al servizio di riparazione auto! Qui potrete esplorare una raccolta di immagini che mostrano il nostro lavoro quotidiano, dalle riparazioni di precisione ai restauri completi. Ogni foto racconta una storia di dedizione e professionalità, evidenziando la qualità del nostro servizio e la soddisfazione dei nostri clienti. Siamo fieri di condividere con voi il nostro impegno per mantenere le vostre auto in perfette condizioni. Buona visione!",
+};
--- src/data/services.ts
+++ src/data/services.ts
@@ -1,31 +1,31 @@
 export const services = {
-	title: 'I Nostri Servizi di Riparazione Auto',
-	paragraph:
-		'Benvenuti nella nostra pagina dei servizi! Siamo orgogliosi di offrire una vasta gamma di servizi di riparazione e manutenzione per la vostra auto. La nostra squadra di meccanici esperti è qui per garantire che la vostra vettura funzioni sempre al meglio. Ecco alcuni dei servizi che offriamo:',
-	cards: [
-		{
-			title: 'Riparazioni',
-			description:
-				'Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.',
-			image: '../assets/garage.jpg',
-		},
-		{
-			title: 'Riparazioni',
-			description:
-				'Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.',
-			image: '../assets/garage.jpg',
-		},
-		{
-			title: 'Riparazioni',
-			description:
-				'Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.',
-			image: '../assets/garage.jpg',
-		},
-		{
-			title: 'Riparazioni',
-			description:
-				'Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.',
-			image: '../assets/garage.jpg',
-		},
-	],
-}
+    title: "I Nostri Servizi",
+    paragraph:
+        "Benvenuti nella nostra pagina dei servizi! Siamo orgogliosi di offrire una vasta gamma di servizi di riparazione e manutenzione per la vostra auto. La nostra squadra di meccanici esperti è qui per garantire che la vostra vettura funzioni sempre al meglio. Ecco alcuni dei servizi che offriamo:",
+    cards: [
+        {
+            title: "Riparazioni",
+            description:
+                "Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.",
+            image: "../assets/garage.jpg",
+        },
+        {
+            title: "Riparazioni",
+            description:
+                "Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.",
+            image: "../assets/garage.jpg",
+        },
+        {
+            title: "Riparazioni",
+            description:
+                "Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.",
+            image: "../assets/garage.jpg",
+        },
+        {
+            title: "Riparazioni",
+            description:
+                "Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.Riparazioni di precisione e manutenzione completa.",
+            image: "../assets/garage.jpg",
+        },
+    ],
+};
